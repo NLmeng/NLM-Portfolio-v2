012b4f383b24f99adee525330cfb430a7b2504e4
Lymeng Naret <lymengnaret@yahoo.com>
2024-08-17 23:18:54 -0700

    init initial overall design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        black: "rgb(35, 36, 38)",
+        purple: "rgb(202, 174, 241)",
+        red: "rgb(246, 87, 81)",
+        orange: "rgb(253, 143, 93)",
+        white: "rgb(255, 255, 255)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        mono: ["var(--font-roboto-mono)"],
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
