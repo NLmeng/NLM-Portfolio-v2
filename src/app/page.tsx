80afe046dbc639d960ca39c8f1c76e920c84762a
Lymeng Naret <lymengnaret@yahoo.com>
2024-10-20 22:28:24 -0700

    add design for oval nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,51 @@
 import HorizontalAccordionSocials from "@/components/HorizontalAccordionSocials";
-import LeftNavigator from "@/components/LeftNavigator";
+import { LeftNavigator, OvalNavigator } from "@/components/Navigator";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-black text-white">
-      <HorizontalAccordionSocials />
-      <LeftNavigator />
-      <main className="w-full flex justify-center items-center flex-col">
-        <section
-          id="about"
-          className="h-screen flex items-center justify-center"
-        >
-          <div className="text-center">
-            <div className="text-orange text-6xl">Lymeng Naret</div>
-            <div className="text-white mt-4">
-              SOME DESCRIPTIONS HERE THAT WILL BE FILLED IN LATER
+    <>
+      <div className="relative flex min-h-screen bg-black text-white">
+        <HorizontalAccordionSocials />
+        <LeftNavigator />
+        <OvalNavigator />
+        <main className="w-full ml-20 flex justify-center items-center flex-col">
+          <section
+            id="about"
+            className="h-screen flex items-center justify-center"
+          >
+            <div className="text-center">
+              <div className="text-orange text-6xl">Lymeng Naret</div>
+              <div className="text-white mt-4">
+                SOME DESCRIPTIONS HERE THAT WILL BE FILLED IN LATER
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
 
-        <section
-          id="experience"
-          className="h-screen flex items-center justify-center"
-        >
-          <div className="flex flex-col space-y-4 text-center">
-            <div className="border border-purple p-4">
-              -Present PLACE HOLDER CARD 1
+          <section
+            id="experience"
+            className="h-screen flex items-center justify-center"
+          >
+            <div className="flex flex-col space-y-4 text-center">
+              <div className="border border-purple p-4">
+                -Present PLACE HOLDER CARD 1
+              </div>
+              <div className="border border-purple p-4">
+                -2023 PLACE HOLDER CARD 2
+              </div>
+              <div className="border border-purple p-4">
+                -2022 PLACE HOLDER CARD 3
+              </div>
             </div>
-            <div className="border border-purple p-4">
-              -2023 PLACE HOLDER CARD 2
-            </div>
-            <div className="border border-purple p-4">
-              -2022 PLACE HOLDER CARD 3
-            </div>
-          </div>
-        </section>
+          </section>
 
-        <section
-          id="projects"
-          className="h-screen flex items-center justify-center"
-        >
-          <div>Projects section content here...</div>
-        </section>
-      </main>
-    </div>
+          <section
+            id="projects"
+            className="h-screen flex items-center justify-center"
+          >
+            <div>Projects section content here...</div>
+          </section>
+        </main>
+      </div>
+    </>
   );
 }
--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -30,7 +30,7 @@ const NavigationItem = ({
   );
 };
 
-export default function LeftNavigator() {
+export function LeftNavigator() {
   const [currentSection, setCurrentSection] = useState("ABOUT");
 
   useEffect(() => {
@@ -62,7 +62,7 @@ export default function LeftNavigator() {
   const sections = ["ABOUT", "EXPERIENCE", "PROJECTS"];
 
   return (
-    <div className="ml-4 fixed left-4 top-1/2 transform -translate-y-1/2 z-40 w-48 flex flex-col items-center">
+    <div className="ml-6 fixed left-6 top-1/2 transform -translate-y-1/2 z-40 w-48 flex flex-col items-center">
       <ul className="space-y-8 text-lg">
         {sections.map((section) => (
           <NavigationItem
@@ -73,6 +73,60 @@ export default function LeftNavigator() {
           />
         ))}
       </ul>
+    </div>
+  );
+}
+export function OvalNavigator() {
+  const outerPerimeter = 420.48;
+  const innerPerimeter = 364.6;
+  const outerGap = 14.016;
+  const innerGap = 14.023;
+  const innerOffset = innerGap / 2;
+
+  return (
+    <div className="animated-oval">
+      <svg width="50" height="200" viewBox="0 0 50 200">
+        <ellipse
+          cx="25"
+          cy="100"
+          rx="20"
+          ry="100"
+          fill="none"
+          stroke="rgb(var(--clean-white))"
+          strokeWidth="7"
+          strokeDasharray={`0,${outerGap}`}
+          strokeLinecap="round"
+        >
+          <animate
+            attributeName="stroke-dashoffset"
+            from="0"
+            to={`${outerPerimeter}`}
+            dur="20s"
+            repeatCount="indefinite"
+            calcMode="linear"
+          />
+        </ellipse>
+        <ellipse
+          cx="25"
+          cy="100"
+          rx="10"
+          ry="90"
+          fill="none"
+          stroke="rgb(var(--main-orange))"
+          strokeWidth="7"
+          strokeDasharray={`0,${innerGap}`}
+          strokeLinecap="round"
+        >
+          <animate
+            attributeName="stroke-dashoffset"
+            from={`${innerPerimeter + innerOffset}`}
+            to={`${innerOffset}`}
+            dur="15s"
+            repeatCount="indefinite"
+            calcMode="linear"
+          />
+        </ellipse>
+      </svg>
     </div>
   );
 }
