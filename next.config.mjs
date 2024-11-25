9d75b30f7695821d4802752ca2308a3d24d19385
Lymeng Naret <lymengnaret@yahoo.com>
2024-11-25 00:16:22 -0800

    Use env to stream S3 to  load pdf without git changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/resume.pdf",
+        destination: "/api/resumepdf",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
